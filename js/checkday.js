0e8ea633ff71bf57d2d2cc060ebe5fb732ca1ae3
m.bach@hq-imaging.de <m.bach@hq-imaging.de>
2022-11-16 17:22:27 +0100

    material added

--- js/checkday.js
+++ js/checkday.js
@@ -1,13 +1,16 @@
+var currentDate = new Date();
+
 function checkday(id) {
 	
   const cbox = document.getElementById(id);
   const vis = document.getElementById(id+'a');
   // alert(id);
   
-    if (cbox.checked == true){
+    if (cbox.checked == true && parseInt(id.slice(1)) <= currentDate.getDate() ){
 		vis.style.opacity = 1;
 		vis.style.transform = 'scale(1.5,1.5)';
 	} else {
+		cbox.checked = false
 		vis.style.opacity = 0;
 	}
   
